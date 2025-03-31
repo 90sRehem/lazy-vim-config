d6afb2991b47b67999f47d0f769609db36a1b7ad
Jonathan Rehem <jonathan.rehem@outlook.com>
2025-03-31 13:08:27 -0300

    feat: add obsidian plugin

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,10 +1,35 @@
 return {
   "folke/which-key.nvim",
-  config = function()
-    require("which-key").setup({
-      window = {
-        position = "top", -- mude para a posição desejada
+  event = "VeryLazy",
+  opts_extend = { "spec" },
+  opts = {
+    defaults = {},
+    spec = {
+      {
+        mode = { "n", "v" },
+        { "<leader>o", group = "Obsidian" },
+        { "<leader>oo", "<cmd>ObsidianOpen<CR>", desc = "Abrir nota atual no Obsidian" },
+        { "<leader>on", "<cmd>ObsidianNew<CR>", desc = "Criar nova nota" },
+        { "<leader>os", "<cmd>ObsidianSearch<CR>", desc = "Buscar notas" },
+        { "<leader>oq", "<cmd>ObsidianQuickSwitch<CR>", desc = "Alternar notas" },
+        { "<leader>of", "<cmd>ObsidianFollowLink<CR>", desc = "Seguir link" },
+        { "<leader>ob", "<cmd>ObsidianBacklinks<CR>", desc = "Ver backlinks" },
+        { "<leader>ot", "<cmd>ObsidianTags<CR>", desc = "Buscar por tags" },
+        { "<leader>od", "<cmd>ObsidianToday<CR>", desc = "Abrir nota diária" },
+        { "<leader>oy", "<cmd>ObsidianYesterday<CR>", desc = "Abrir nota de ontem" },
+        { "<leader>om", "<cmd>ObsidianTomorrow<CR>", desc = "Abrir nota de amanhã" },
+        { "<leader>ol", "<cmd>ObsidianLinks<CR>", desc = "Listar links da nota" },
+        { "<leader>op", "<cmd>ObsidianPasteImg<CR>", desc = "Colar imagem" },
+        { "<leader>or", "<cmd>ObsidianRename<CR>", desc = "Renomear nota" },
+        { "<leader>ox", "<cmd>ObsidianExtractNote<CR>", desc = "Extrair texto para nova nota" },
       },
-    })
+    },
+    window = {
+      position = "top", -- mantém a posição da janela como na configuração original
+    },
+  },
+  config = function(_, opts)
+    local wk = require("which-key")
+    wk.setup(opts)
   end,
 }
