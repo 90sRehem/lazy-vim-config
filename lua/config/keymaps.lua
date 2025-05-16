16e774d78d415cbbc2226decd22a31eb6c793a38
Jonathan Rehem <jonathan.rehem@outlook.com>
2025-05-16 15:30:59 -0300

    add keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,4 +1,16 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- open a new empty buffer
 vim.keymap.set("n", "<leader>bn", ":enew<CR>", { desc = "Novo buffer vazio" })
+-- Vai para a definição em uma split vertical
+vim.keymap.set("n", "gv", function()
+  vim.cmd("vsplit") -- Abre uma vertical split
+  vim.lsp.buf.definition() -- Vai para a definição com LSP
+end, { desc = "Go to definition in vertical split" })
+
+-- VISUAL MODE
+-- move selected block of text up and down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected block down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected block up" })
