48948fe3ef4ce80a6e99af83bfe33c30c15a3f0a
Jonathan Rehem <jonathan.rehem@outlook.com>
2025-07-30 15:33:40 -0300

    update lspconffig

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -13,19 +13,6 @@ return {
   end,
   opts = {
     servers = {
-      eslint = {
-        settings = {
-          workingDirectories = { mode = "auto" },
-          format = { enable = true },
-          lint = { enable = true },
-        },
-        on_attach = function(client, bufnr)
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            buffer = bufnr,
-            command = "EslintFixAll",
-          })
-        end,
-      },
       tsserver = {},
       tailwindcss = {},
       marksman = {},
@@ -56,15 +43,6 @@ return {
       },
     },
     setup = {
-      eslint = function()
-        require("lazyvim.util").lsp.on_attach(function(client)
-          if client.name == "eslint" then
-            client.server_capabilities.documentFormattingProvider = true
-          elseif client.name == "tsserver" then
-            client.server_capabilities.documentFormattingProvider = false
-          end
-        end)
-      end,
       clangd = function(_, opts)
         opts.capabilities.offsetEncoding = { "utf-16" }
       end,
